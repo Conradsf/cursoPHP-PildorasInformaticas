17ef360e779d16849eca12c2a6babfbbf527dab6
Conrad Soler Ferrer <conradsf@gmail.com>
2020-05-22 02:10:44 +0200

    Primer video subido :) "Hola Mundo"

---
+++ video-03/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=< initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        print "Bienvenidos al curso PHP <br>";
+        print "Hola alumnos <br>";
+        print "Hasta el próximo curso <br>";
+    ?>
+</body>
+</html>
